e4b3d3078bd1d1b1aa31cbaffe5931074821fda7
Rohit Kumar <rohit2kumar02@gmail.com>
2026-03-23 13:47:59 +0530

    add render url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8789"
+  baseURL: "https://java-test-lab.onrender.com"
 });
 
 api.interceptors.request.use((config) => {
